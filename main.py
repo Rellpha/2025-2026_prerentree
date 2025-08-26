75d2ff53cf58d47eb7f8f4b61e2b0a5a0b072c61
Pharell <rellpha0@gmail.com>
2025-08-26 16:19:20 +0200

    Ajout d'une fonction

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 # La cible
 import random
+
+def verifie(prix_a_trouver,essai):
+    if prix_a_trouver == essai:
+        print("Bravo !")
+        return 0
+    elif prix_a_trouver < essai:
+        print("Trop élevé...")
+        return 1
+    else:
+        print("Trop peu...")
+        return -1
+
 prix_a_trouver = random.randint(1,10)
 print(f"Prix à trouver : {prix_a_trouver}")
 
-# L'essai
-essai = input("Votre proposition (entre 1 et 10) : ")
-essai = int(essai)
+for i in range(1, 6):
 
-#print(f"Type de prix_a_trouver : {type(prix_a_trouver)}")
-#print(f"Type de essai : {type(essai)}")
+    # L'essai
+    essai = input(f"Essai n°{i} : Votre proposition (entre 1 et 10) : ")
+    try:    
+        essai = int(essai)
+    except ValueError as exeption:
+        print("Erreur de saisie...")
+        print(f"Message de Python: {exeption}")
+        continue
+        
+    #print(f"Type de prix_a_trouver : {type(prix_a_trouver)}")
+    #print(f"Type de essai : {type(essai)}")
 
-# La comparaison
-if prix_a_trouver == essai :
-    print("Bravo !")
-elif prix_a_trouver < essai :
-    print("Trop élévé...")
-else:
-    print("Trop peu...")
+    # La comparaison
+    if verifie(prix_a_trouver,essai)==0:
+        break
+    
+
+print("Fin...")
